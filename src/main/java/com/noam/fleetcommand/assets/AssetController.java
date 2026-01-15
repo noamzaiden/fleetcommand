7463f7510346ad51fd7867af0a2891381f48b07a
noamzaiden <noamzaiden@gmail.com>
2026-01-15 20:33:42 +0100

    Add asset REST controller

---
+++ src/main/java/com/noam/fleetcommand/assets/AssetController.java
@@ -0,0 +1,41 @@
+package com.noam.fleetcommand.assets;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/assets")
+public class AssetController {
+
+    private final AssetService assetService;
+
+    public AssetController(AssetService assetService) {
+        this.assetService = assetService;
+    }
+
+    @GetMapping
+    public List<Asset> getAllAssets() {
+        return assetService.getAllAssets();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Asset> getAssetById(@PathVariable Long id) {
+        return assetService.getAssetById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Asset> createAsset(@RequestBody Asset asset) {
+        Asset createdAsset = assetService.createAsset(asset);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAsset);
+    }
+}
