8ad27a7f8218212c224d6927c8e71b9fa9af5781
Noam Zaiden <noamzaiden@gmail.com>
2026-01-15 19:40:34 +0100

    Add Asset JPA entity mapped to assets table

---
+++ src/main/java/com/noam/fleetcommand/assets/Asset.java
@@ -0,0 +1,84 @@
+package com.noam.fleetcommand.assets;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "assets")
+public class Asset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "tracking_enabled", nullable = false)
+    private Boolean trackingEnabled = false;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "last_heartbeat")
+    private LocalDateTime lastHeartbeat;
+
+    public Asset() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getTrackingEnabled() {
+        return trackingEnabled;
+    }
+
+    public void setTrackingEnabled(Boolean trackingEnabled) {
+        this.trackingEnabled = trackingEnabled;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public LocalDateTime getLastHeartbeat() {
+        return lastHeartbeat;
+    }
+
+    public void setLastHeartbeat(LocalDateTime lastHeartbeat) {
+        this.lastHeartbeat = lastHeartbeat;
+    }
+
+
+}
